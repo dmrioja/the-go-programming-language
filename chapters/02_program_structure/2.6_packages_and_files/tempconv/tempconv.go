dbead098d9680869d601a17a32325a19b91436af
dmunoz <d.mrioja@gmail.com>
2025-04-18 11:41:03 +0200

    feat: chapter 2.6 packages and files

---
+++ chapters/02_program_structure/2.6_packages_and_files/tempconv/tempconv.go
@@ -0,0 +1,25 @@
+// Package tempconv performs Celsius and Fahrenheit conversions.
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+
+	AbsoluteZeroF Fahrenheit = -459.67
+	FreezingF     Fahrenheit = 32
+	BoilingF      Fahrenheit = 212
+)
+
+func (c Celsius) String() string {
+	return fmt.Sprintf("%gºC", c)
+}
+
+func (f Fahrenheit) String() string {
+	return fmt.Sprintf("%gºF", f)
+}
