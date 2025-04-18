dcb6a7db4104ee62f3728509e398a58a4aee81af
dmunoz <d.mrioja@gmail.com>
2025-04-18 11:51:37 +0200

    feat: chapter 2.6 exercises

--- chapters/02_program_structure/2.6_packages_and_files/tempconv/tempconv.go
+++ chapters/02_program_structure/2.6_packages_and_files/tempconv/tempconv.go
@@ -1,19 +1,24 @@
-// Package tempconv performs Celsius and Fahrenheit conversions.
+// Package tempconv performs Celsius, Fahrenheit and Kelvin conversions.
 package tempconv
 
 import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
-	AbsoluteZeroC Celsius = -273.15
-	FreezingC     Celsius = 0
-	BoilingC      Celsius = 100
+	AbsoluteZeroC  Celsius = -273.15
+	WaterFreezingC Celsius = 0
+	WaterBoilingC  Celsius = 100
 
-	AbsoluteZeroF Fahrenheit = -459.67
-	FreezingF     Fahrenheit = 32
-	BoilingF      Fahrenheit = 212
+	AbsoluteZeroF  Fahrenheit = -459.67
+	WaterFreezingF Fahrenheit = 32
+	WaterBoilingF  Fahrenheit = 212
+
+	AbsoluteZeroK  Kelvin = 0
+	WaterFreezingK Kelvin = 273.15
+	WaterBoilingK  Kelvin = 373.15
 )
 
 func (c Celsius) String() string {
@@ -22,4 +27,8 @@ func (c Celsius) String() string {
 
 func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%gºF", f)
+}
+
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%gK", k)
 }
